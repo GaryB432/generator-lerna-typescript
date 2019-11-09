2f204198d85dfef0037ffb3b3fe2eab963dd27f7
Gary Bortosky <garybort@gmail.com>
2019-11-09 15:38:02 -0600

    add version to generated package

--- generators/package/index.js
+++ generators/package/index.js
@@ -17,6 +17,31 @@ module.exports = class extends Generator {
     const context = {
       packageName: Case.kebab(this.options.packageName)
     };
+
+    const lernaJson = this.fs.readJSON("lerna.json", {
+      version: "independent"
+    });
+
+    const packageJson = {
+      name: context.packageName,
+      version: lernaJson.version === "independent" ? "0.0.0" : lernaJson,
+      description: "",
+      files: ["lib"],
+      private: false,
+      main: "lib/index.js",
+      typings: "lib/index.d.ts",
+      scripts: {
+        prepare: "npm run build",
+        build: "tsc --pretty"
+      },
+      keywords: [],
+      author: "",
+      license: "ISC",
+      devDependencies: {
+        typescript: "^3.2.2"
+      }
+    };
+
     const pfn = fname => path.join("packages", context.packageName, fname);
     this.fs.copyTpl(
       this.templatePath("__tests__/index.spec.ts"),
@@ -28,11 +53,9 @@ module.exports = class extends Generator {
       this.destinationPath(pfn("src/index.ts")),
       context
     );
-    this.fs.copyTpl(
-      this.templatePath("_package.json"),
-      this.destinationPath(pfn("package.json")),
-      context
-    );
+
+    this.fs.extendJSON(pfn("package.json"), packageJson);
+
     this.fs.copyTpl(
       this.templatePath("_tsconfig.json"),
       this.destinationPath(pfn("tsconfig.json")),
